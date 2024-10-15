515c6b4855c30d3c50adbb38863b50b2648482a3
Luis Henrique <lh5818181@gmail.com>
2024-10-15 15:46:53 -0300

    modificando arrays

--- dist/Arrays.js
+++ dist/Arrays.js
@@ -23,3 +23,38 @@ var indiceDoPaulo = alunos2.findIndex(function (item) {
   return item.nome == 'paulo';
 });
 console.log(indiceDoPaulo);
+alunos2.push({
+  nome: 'lucio',
+  curso: 'Backend'
+});
+
+//every
+var todosAlunosDoFrontend = alunos2.every(function (item) {
+  return item.curso === 'Frontend';
+});
+console.log("".concat(todosAlunosDoFrontend, " <-- func\xE7\xE3o para verificar a existencia de alunos frontend"));
+var alunosExistentesDoBackend = alunos2.some(function (item) {
+  return item.curso === 'Backend' && item.curso === 'Frontend';
+});
+console.log("".concat(alunosExistentesDoBackend, " <-- func\xE7\xE3o para verificar a existencia de alunos fullstack"));
+function filtraAlunosDoBackend(aluno) {
+  return aluno.curso === 'Backend';
+}
+var alunosDoBackend = alunos2.filter(filtraAlunosDoBackend);
+console.log(alunosDoBackend);
+var nums = [10, 20, 30, 40];
+var soma = nums.reduce(function (acumulador, itemAtual) {
+  acumulador += itemAtual;
+  return acumulador;
+}, 0);
+console.log(soma);
+var somaComFor = 0;
+for (var _i = 0; _i < nums.length; _i++) {
+  somaComFor += nums[_i];
+}
+console.log(somaComFor);
+var nomesDosAlunos = alunos2.reduce(function (acumulador, itemAtual) {
+  acumulador += "".concat(itemAtual.nome, ", ");
+  return acumulador;
+}, '');
+console.log(nomesDosAlunos);
--- src/Arrays.js
+++ src/Arrays.js
@@ -20,7 +20,7 @@ const alunos2 = alunos.map(function(itemAtual) {
 console.log(alunos2);
 
 
-const paulo = alunos2.find(function(item) {
+const paulo = alunos2.find(function(item) { //faz a busca de um item em um array:
     return item.nome == 'paulo'
 })
 
@@ -31,3 +31,53 @@ const indiceDoPaulo = alunos2.findIndex(function(item) {
 })
 
 console.log(indiceDoPaulo);
+
+alunos2.push({
+    nome:'lucio',
+    curso: 'Backend'
+})
+
+//every
+const todosAlunosDoFrontend = alunos2.every(function(item) {
+    return item.curso === 'Frontend'
+})
+
+console.log(`${todosAlunosDoFrontend} <-- funcção para verificar a existencia de alunos frontend`);
+
+const alunosExistentesDoBackend = alunos2.some(function(item) {
+    return item.curso === 'Backend' && item.curso === 'Frontend'
+})
+
+console.log(`${alunosExistentesDoBackend} <-- funcção para verificar a existencia de alunos fullstack`);
+
+function filtraAlunosDoBackend(aluno) {
+    return aluno.curso === 'Backend';
+}
+
+const alunosDoBackend = alunos2.filter(filtraAlunosDoBackend)
+
+console.log(alunosDoBackend); 
+
+const nums = [10, 20, 30, 40];
+
+const soma = nums.reduce(function(acumulador, itemAtual) {
+    acumulador += itemAtual;
+    return acumulador;
+}, 0)
+
+console.log(soma);
+
+let somaComFor = 0;
+
+for (let i = 0; i < nums.length; i++) {
+    somaComFor += nums[i];
+}
+
+console.log(somaComFor);
+
+const nomesDosAlunos = alunos2.reduce(function(acumulador, itemAtual) {
+    acumulador += `${itemAtual.nome}, `;
+    return acumulador;
+}, '')
+
+console.log(nomesDosAlunos);
